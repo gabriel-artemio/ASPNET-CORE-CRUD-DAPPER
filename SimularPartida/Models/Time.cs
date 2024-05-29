578bbb153f34c87316ee78123409684865ad3101
Gabriel Artemio <43358004+gabriel-artemio@users.noreply.github.com>
2024-05-28 23:07:21 -0300

    projeto simulador de partidas

---
+++ SimularPartida/Models/Time.cs
@@ -0,0 +1,17 @@
+﻿namespace SimularPartida.Models
+{
+    public class Time
+    {
+        public string? Nome { get; set; }
+        public List<Jogador> Jogadores { get; set; }
+        public int FormaRecente { get; set; }
+        public int Motivacao { get; set; }
+        public string? Formacao { get; set; }
+
+        public Time(string nome)
+        {
+            Nome = nome;
+            Jogadores = new List<Jogador>();
+        }
+    }
+}
